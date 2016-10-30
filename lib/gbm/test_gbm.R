a05b0c494bfd38bba53f98b4a32c6543634ef2e4
younhyukcho <yc3074@columbia.edu>
2016-10-30 04:16:44 -0400

    Add files via upload

---
+++ lib/gbm/test_gbm.R
@@ -0,0 +1,7 @@
+test<-function(fit_train,dat_test) {
+  library("gbm")
+  pred<-predict(fit_train$fit,newdata=dat_test,n.trees=fit_train$iter,type="response")
+  
+  return(as.numeric(pred>0.5))
+}
+
---
+++ lib/gbm/train_gbm.R
@@ -0,0 +1,18 @@
+train<-function(dat_train,label_train,par=NULL) {
+  library(gbm)
+  
+  if(is.null(par)){
+    depth <- 3
+  } else {
+    depth <- par$depth
+  }
+  fit_gbm <- gbm.fit(x=dat_train, y=label_train,
+                     n.trees=300,
+                     distribution="bernoulli",
+                     interaction.depth=depth,
+                     bag.fraction = 0.5,
+                     verbose=FALSE)
+  best_iter <- gbm.perf(fit_gbm, method="OOB")
+  
+  return(list(fit=fit_gbm, iter=best_iter))
+}
