c48b434dcbaac16f4122599448ed12f50b04107d
Chang Liu <cl3388@columbia.edu>
2016-10-30 19:07:06 -0400

    Update train_tree.R

--- lib/tree/train_tree.R
+++ lib/tree/train_tree.R
@@ -1,7 +1,7 @@
 train <- function(dat_train,label_train){
   library(rpart)
   
-  mycontrol = rpart.control(cp = 0, xval = 10)
+  mycontrol = rpart.control(cp = 0, xval = 5)
   fittree = rpart(label_train~.,method = "class",data = dat_train,control = mycontrol)
   fittree$cptable
   cptarg = fittree$cptable[which(fittree$cptable[,4]==min(fittree$cptable[,4])),1]
