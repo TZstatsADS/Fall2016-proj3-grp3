9f142ea16f29c2b176245a0e2e5b063a6d1e4ed4
Chang Liu <cl3388@columbia.edu>
2016-10-30 17:14:18 -0400

    Add files via upload

---
+++ lib/train_tree.R
@@ -0,0 +1,11 @@
+train <- function(dat_train,label_train){
+  library(rpart)
+  
+  mycontrol = rpart.control(cp = 0, xval = 10)
+  fittree = rpart(label_train~.,method = "class",data = dat_train,control = mycontrol)
+  fittree$cptable
+  cptarg = fittree$cptable[which(fittree$cptable[,4]==min(fittree$cptable[,4])),1]
+  prunedtree = prune(fittree,cp=cptarg)
+  
+  return(prunedtree)
+}
