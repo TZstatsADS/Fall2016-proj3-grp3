a600ea5f50a9af922188701f1daa1923a3ac9f20
Chang Liu <cl3388@columbia.edu>
2016-10-30 19:39:28 -0400

    Update train_tree.R

--- lib/tree/train_tree.R
+++ lib/tree/train_tree.R
@@ -2,7 +2,7 @@ train <- function(dat_train,label_train){
   library(rpart)
   
   mycontrol = rpart.control(cp = 0, xval = 5)
-  fittree = rpart(label_train~.,method = "class",data = dat_train,control = mycontrol)
+  fittree = rpart(label_train ~ dat_train,method = "class",control = mycontrol)
   cptarg = fittree$cptable[which(fittree$cptable[,4]==min(fittree$cptable[,4])),1]
   prunedtree = prune(fittree,cp=cptarg)
   
