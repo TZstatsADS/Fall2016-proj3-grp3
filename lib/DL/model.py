c84bc471662323ccec228878a11ce762c33b8b2b
Wenhang Bao <wb2304@columbia.edu>
2016-10-31 18:37:12 -0400

    Create model.py

---
+++ lib/DL/model.py
@@ -0,0 +1,105 @@
+import os
+import h5py
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+# path to the model weights file.
+weights_path = '/Users/mac/Fall 2016/GR5243/Project3/vgg16_weights.h5'
+top_model_weights_path = 'bottleneck_fc_model.h5'
+# dimensions of our images.
+img_width, img_height = 150, 150
+test_data_dir='/Users/mac/Fall 2016/GR5243/Project3/Project3_poodleKFC_train/data/test'
+nb_test_samples = 3
+nb_epoch = 50
+
+
+
+datagen = ImageDataGenerator(rescale=1./255)
+# build the VGG16 network
+model = Sequential()
+model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
+
+model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    # load the weights of the VGG16 networks
+    # (trained on ImageNet, won the ILSVRC competition in 2014)
+    # note: when there is a complete match between your model definition
+    # and your weight savefile, you can simply call model.load_weights(filename)
+assert os.path.exists(weights_path), 'Model weights not found (see "weights_path" variable in script).'
+f = h5py.File(weights_path)
+for k in range(f.attrs['nb_layers']):
+    if k >= len(model.layers):
+            # we don't look at the last (fully-connected) layers in the savefile
+        break
+    g = f['layer_{}'.format(k)]
+    weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+    model.layers[k].set_weights(weights)
+f.close()
+print('Model loaded.')
+
+generator = datagen.flow_from_directory(test_data_dir, batch_size = 32, target_size =(img_width, img_height), class_mode = None, shuffle =False)
+bottleneck_features_test = model.predict_generator(generator, nb_test_samples)
+np.save(open('bottleneck_features_test.npy','wb'), bottleneck_features_test)
+
+
+train_data = np.load(open('bottleneck_features_train.npy'))
+train_labels = np.array([1] * (nb_train_samples / 2) + [0] * (nb_train_samples / 2))
+
+validation_data = np.load(open('bottleneck_features_validation.npy'))
+validation_labels = np.array([1] * (nb_validation_samples / 2) + [0] * (nb_validation_samples / 2))
+
+
+model2 = Sequential()
+model2.add(Flatten(input_shape=train_data.shape[1:]))
+model2.add(Dense(256, activation='relu'))
+model2.add(Dropout(0.5))
+model2.add(Dense(1, activation='sigmoid'))
+model2.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+model2.fit(train_data, train_labels,
+              nb_epoch=nb_epoch, batch_size=32,
+              validation_data=(validation_data, validation_labels))
+
+
+test_data = np.load(open('bottleneck_features_test.npy', 'rb'))
+model2.predict_classes(test_data)
+
+
+
+
