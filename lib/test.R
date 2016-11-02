379918ccfe4d629c02c3eec7259a1688469aa686
younhyukcho <yc3074@columbia.edu>
2016-11-02 17:56:11 -0400

    Update test.R

--- lib/test.R
+++ lib/test.R
@@ -1,6 +1,6 @@
 test<-function(fit_train,dat_test) {
   library("gbm")
-  pred<-predict(fit_train,newdata=dat_test,n.trees=fit_train$iter,type="response")
+  pred<-predict(fit_train$fit,newdata=dat_test,n.trees=fit_train$iter,type="response")
   
   return(as.numeric(pred>0.5))
 }
